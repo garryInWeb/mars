cbd9a10ce5d558f6d7c73e45b128269e3eb7391a
ivanberry <ivanberry@outlook.com>
2017-04-15 10:32:05 +0800

    Update frontend tasks and file structure into develop branch

--- tasks/compless.js
+++ tasks/compless.js
@@ -1,10 +1,14 @@
+var path = require('path');
+
 module.exports = function (gulp,options,plugins) {
+
     const LessAutoprefix = plugins.autoprofixer;
     const autoprefixer = new LessAutoprefix({ browsers: ['last 3 version']});
     const cache = plugins.cache;
+    const baseDir = options.paths.baseDir[0];
 
     gulp.task('compless', function () {
-        return gulp.src(options.paths.less)
+        return gulp.src(['./source/less/**/*.less', '!source/less/bootstrap/**/*.less', '!source/less/font-awesome/**/*.less'])
             .pipe(cache('complessing'))
             .pipe(plugins.sourcemaps.init())
             .pipe(plugins.less({
@@ -13,6 +17,6 @@ module.exports = function (gulp,options,plugins) {
             .pipe(plugins.cssmin())
             .pipe(plugins.rename({suffix:'.min'}))
             .pipe(plugins.sourcemaps.write('./',{addComment:false}))
-            .pipe(gulp.dest('template/default/css'))
+            .pipe(gulp.dest(baseDir + 'css'))
     });
 };
